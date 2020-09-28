c1dcb2927e29ac1e78adc37e69c4fcaa14703e8b
abi83 <GFhjkm83>
2020-09-28 14:53:37 +0700

    Saving

--- app/posts/admin.py
+++ app/posts/admin.py
@@ -3,14 +3,12 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     # перечисляем поля, которые должны отображаться в админке
-    list_display = ("text", "pub_date", "author")
+    list_display = ("pk", "text", "pub_date", "author")
     # добавляем интерфейс для поиска по тексту постов
     search_fields = ("text",)
     # добавляем возможность фильтрации по дате
     list_filter = ("pub_date",)
 
 admin.site.register(Post, PostAdmin)
-
-
 
 # Register your models here.
