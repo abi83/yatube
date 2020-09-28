f019b489dfa29e4723516663c98d3829c6defd64
abi83 <GFhjkm83>
2020-09-28 16:51:05 +0700

    clearing cache

--- app/posts/admin.py
+++ app/posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
+
 class PostAdmin(admin.ModelAdmin):
     # перечисляем поля, которые должны отображаться в админке
     list_display = ("pk", "text", "pub_date", "author")
@@ -8,6 +9,7 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ("text",)
     # добавляем возможность фильтрации по дате
     list_filter = ("pub_date",)
+
 
 admin.site.register(Post, PostAdmin)
 
--- app/posts/models.py
+++ app/posts/models.py
@@ -7,4 +7,6 @@ User = get_user_model()
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name="posts")
